54375a8c1f33507e6c419ced91d4a61e7e4ee71a
Emilio Torres Jr <emilio@reclyptor.com>
2024-04-08 05:23:59 -0500

    Installed Dependencies
    tailwindcss clsx prettier

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { MetaFunction, LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
 
 export const meta: MetaFunction = () => [
   { charSet: "utf-8" },
@@ -17,6 +18,7 @@ export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
   { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" },
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
+
